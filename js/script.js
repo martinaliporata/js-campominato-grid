f4fcc262be737d283e8710af1e183aabce3d6750
martinaliporata <martinaliporata@gmail.com>
2024-05-20 21:31:54 +0200

    aggiunto select per selezionare la difficoltà, lo switch per cambiare il numero di celle in base alla difficoltà

--- js/script.js
+++ js/script.js
@@ -3,31 +3,73 @@ const gridElement = document.querySelector('section#grid')
 const playButton = document.getElementById('play-button')
 
 playButton.addEventListener('click', function(){
-    generateNewGame(gridElement);
+    generateNewGame(gridElement, difficultySelectEl);
 })
+
+const difficultySelectEl = document.querySelector('#select-difficulty')
 
 function generateNewGame(containerElement, selectElement) {
     containerElement.innerHTML = '';
     
+    let numberOfCells
+    let classToAdd
+
+    switch (selectElement.value) {
+    case '0':
+        numberOfCells = 100;
+        classToAdd='easy';
+        break;
+    case '1':
+        numberOfCells = 81;
+        classToAdd='medium';
+        break;
+    case '2':
+        numberOfCells = 49;
+        classToAdd='hard';
+        break;
+    default:
+        numberOfCells = 100;
+        classToAdd='easy';
+    }
+
     for(let index = 0; index < numberOfCells; index++) {
-        const squareElement = document.createElement('article')
-        squareElement.classList.add('square', classToAdd)
+        const squareElement = document.createElement('article');
+        squareElement.classList.add('square', classToAdd);
 
         const contentEl = document.createElement('span')
-        contentEl.classList.add('content')
-        contentEl.append(index+1)
-        squareElement.appendChild(contentEl)
+        contentEl.classList.add('content');
+        contentEl.append(index+1);
+        squareElement.appendChild(contentEl);
 
         squareElement.addEventListener('click', function(){
             if((index+1) % 2 === 0){
-                console.log(index)
+                console.log(index);
                 squareElement.classList.add('active');
             } else {
-                console.log(index)
-                squareElement.classList.add('active', 'odd')
+                console.log(index);
+                squareElement.classList.add('active', 'odd');
             }
         });
 
         gridElement.appendChild(squareElement);
     }
+}
+
+function getRandomInt(min,max) {
+    return Math.floor(Math.random()*((max + 1) - min)) + min
+}
+
+function getRandomIntUnique(uninvitedNumbers, min,max) {
+    let randomNumber
+    let isFound = false
+
+    while (isFound){
+        randomNumber = getRandomInt(min, max)
+
+        if(uninvitedNumbers.includes(randomNumber) !== false){
+            isFound = true
+        }
+    }
+
+    return randomNumber
 }
